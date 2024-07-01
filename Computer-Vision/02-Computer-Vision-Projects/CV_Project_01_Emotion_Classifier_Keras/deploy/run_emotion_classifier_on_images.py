c8050c58f79707c87cd0c73b0d26b9e47c44a256
ancilcleetus <ancilcleetus.work@gmail.com>
2024-07-01 08:14:05 +0530

    Corrected 'model_path'

--- Computer-Vision/02-Computer-Vision-Projects/CV_Project_01_Emotion_Classifier_Keras/deploy/run_emotion_classifier_on_images.py
+++ Computer-Vision/02-Computer-Vision-Projects/CV_Project_01_Emotion_Classifier_Keras/deploy/run_emotion_classifier_on_images.py
@@ -10,15 +10,13 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from taipy.gui import Gui, notify
 
 # Load your pre-trained model
-# model_path = "ResNet50_Transfer_Learning_40_Epochs.keras"
-model_path = "Computer-Vision/02-Computer-Vision-Projects/CV_Project_01_Emotion_Classifier_Keras/deploy/ResNet50_Transfer_Learning_40_Epochs.keras"
-model = tf.keras.models.load_model(model_path)
+model = tf.keras.models.load_model("ResNet50_Transfer_Learning_40_Epochs.keras")
 
 # Emotion labels
 emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
 
 # Initialize the face classifier
-face_classifier = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+face_classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 
 # Function to predict emotion
